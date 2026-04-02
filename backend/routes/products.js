c6008f4ac8ce65cf938d7610b3e8bc577982e41d
ashishsonawane6969-afk <ashishsonawane6969@gmail.com>
2026-04-02 17:02:48 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const router  = express.Router();
 
-console.log("🔍 Loading productController...");
+
 
 const ctrl = require('../controllers/productController');
 
-console.log("✅ productController loaded");
+
 
 const auth = require('../middleware/auth');
 const { authenticatedLimiter } = require('../middleware/rateLimiter');
