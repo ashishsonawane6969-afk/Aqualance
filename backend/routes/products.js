aeda1689ce45d85f046293755706126008c3777f
ashishsonawane6969-afk <ashishsonawane6969@gmail.com>
2026-04-02 14:16:20 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -5,7 +5,7 @@ const ctrl = require('../controllers/productController');
 // const auth = require('../middleware/auth');
 const { authenticatedLimiter } = require('../middleware/rateLimiter');
 const { validate } = require('../middleware/validate');
-// const { productWriteSchema, productQuerySchema } = require('../validation/schemas');
+const { productWriteSchema, productQuerySchema } = require('../validation/schemas');
 
 router.get('/', (req, res) => {
   res.json({ message: "route working" });
